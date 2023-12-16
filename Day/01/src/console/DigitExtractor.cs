ce3b29b02f99c685f1f0fc1daa0771fadc0d7465
Christopher Kyle Horton <3268365+Xyaneon@users.noreply.github.com>
2023-12-15 20:51:50 -0500

    Day 1: Add support for Part Two

--- Day/01/src/console/DigitExtractor.cs
+++ Day/01/src/console/DigitExtractor.cs
@@ -1,11 +1,38 @@
+using System.Text.RegularExpressions;
+
 class DigitExtractor
 {
+    private static readonly Dictionary<string, int> digits = new Dictionary<string, int>
+    {
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"nine", 9},
+    };
+
+    private const string digitRegex = "([0-9]|zero|one|two|three|four|five|six|seven|eight|nine)";
+
+    private static Regex firstDigitRegex = new Regex(digitRegex);
+    private static Regex lastDigitRegex = new Regex(digitRegex, RegexOptions.RightToLeft);
+
     public static int GetFirstDigit(string line) =>
-        ConvertDigitToInt(line.Where(x => char.IsDigit(x)).First());
+        ConvertDigitToInt(GetFirstDigitString(line));
 
     public static int GetLastDigit(string line) =>
-        ConvertDigitToInt(line.Where(x => char.IsDigit(x)).Last());
+        ConvertDigitToInt(GetLastDigitString(line));
 
-    private static int ConvertDigitToInt(char digit) =>
-        int.Parse(digit.ToString());
+    private static string GetFirstDigitString(string line) =>
+        firstDigitRegex.Match(line).Groups[1].ToString();
+
+    private static string GetLastDigitString(string line) =>
+        lastDigitRegex.Match(line).Groups[1].ToString();
+
+    private static int ConvertDigitToInt(string digit) =>
+        digit.Length == 1 ? int.Parse(digit) : digits[digit];
 }
