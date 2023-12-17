5483bffc220c71d56ae0c0cff742bdc1cae3e9f6
Christopher Kyle Horton <3268365+Xyaneon@users.noreply.github.com>
2023-12-17 15:23:31 -0500

    Day 4: Implement scratchcard parsing

--- Day/04/src/console/Program.cs
+++ Day/04/src/console/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 1)
+{
+    Console.Error.WriteLine($"Incorrect number of arguments (expected 1, got {args.Length})");
+    Console.Error.WriteLine("Usage: dotnet Day4.exe input-file");
+    return 1;
+}
+
+string[] lines;
+
+try
+{
+    lines = File.ReadAllLines(args[0]);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Failed to read input file: {ex.Message}");
+    return 2;
+}
+
+IEnumerable<Scratchcard> scratchcards = lines.Select(line => ScratchcardParser.Parse(line));
+
+OutputWriter.PrintScratchcards(scratchcards);
+
+// TODO
+
+return 0;
+
+static class OutputWriter
+{
+    public static void PrintScratchcards(IEnumerable<Scratchcard> scratchcards)
+    {
+        foreach (Scratchcard scratchcard in scratchcards)
+        {
+            Console.WriteLine("Card {0}: {1} | {2}",
+                              scratchcard.CardNumber,
+                              string.Join(' ', scratchcard.WinningNumbers),
+                              string.Join(' ', scratchcard.NumbersYouHave));
+        }
+    }
+}
---
+++ Day/04/src/console/ScratchcardParser.cs
@@ -0,0 +1,22 @@
+static class ScratchcardParser
+{
+    public static Scratchcard Parse(string str)
+    {
+        string[] labeledParts = str.Split(':', StringSplitOptions.TrimEntries);
+        string[] numberParts = labeledParts[1].Split('|', StringSplitOptions.TrimEntries);
+
+        int cardNumber = ParseCardNumber(labeledParts[0]);
+        List<int> winningNumbers = ParseNumberList(numberParts[0]);
+        List<int> numbersYouHave = ParseNumberList(numberParts[1]);
+
+        return new Scratchcard(cardNumber, winningNumbers, numbersYouHave);
+    }
+
+    private static int ParseCardNumber(string labelText) =>
+        int.Parse(labelText.Split(' ', StringSplitOptions.TrimEntries).Last());
+
+    private static List<int> ParseNumberList(string numbersText) =>
+        numbersText.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(numberText => int.Parse(numberText))
+            .ToList();
+}
