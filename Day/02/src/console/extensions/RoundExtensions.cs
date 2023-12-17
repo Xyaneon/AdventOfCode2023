47dac512da0b8f7cabaaee4d2ed4f9bc10dcb074
Christopher Kyle Horton <3268365+Xyaneon@users.noreply.github.com>
2023-12-17 01:25:09 -0500

    Day 2: Fix bug in determining possible rounds

--- Day/02/src/console/extensions/RoundExtensions.cs
+++ Day/02/src/console/extensions/RoundExtensions.cs
@@ -3,5 +3,5 @@ namespace Extensions;
 static class RoundExtensions
 {
     public static bool IsPossible(this Round round, int red, int green, int blue) =>
-        red > round.RedCubeCount && green > round.GreenCubeCount && blue > round.BlueCubeCount;
+        red >= round.RedCubeCount && green >= round.GreenCubeCount && blue >= round.BlueCubeCount;
 }
